34bb352ce0f18d44e62ace84ad5f017b382f3faa
José Cuentas Turpo <jcuentast@gmail.com>
2016-01-08 12:47:33 -0500

    commit init

---
+++ src/main/java/com/jcuentas/app/HelloController.java
@@ -0,0 +1,34 @@
+package com.jcuentas.app;
+
+import com.jcuentas.app.model.Hello;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by jcuentas on 08/01/2016.
+ */
+
+@EnableAutoConfiguration
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public Hello hello() {
+        return new Hello("Hello World!");
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public String greetings(@RequestBody String name){
+        return "Hello "+ name;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloController.class, args);
+    }
+
+}
---
+++ src/main/java/com/jcuentas/app/model/Hello.java
@@ -0,0 +1,20 @@
+package com.jcuentas.app.model;
+
+/**
+ * Created by jcuentas on 08/01/2016.
+ */
+public class Hello {
+    private String message;
+
+    public Hello(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
